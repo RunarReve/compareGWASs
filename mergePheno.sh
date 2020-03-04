cca9e197e50ecb4b32ce2ef7001a83e69fa2f63f
RunarReve <reverunar@outlook.com>
2020-03-04 17:42:38 +0300

    Added making of Manhattan plots to merge pheno test

--- mergePheno.sh
+++ mergePheno.sh
@@ -25,16 +25,26 @@ cat ${2} | awk '{if($3 != 2){print $0}}' >> ${out}phe
 sort -k1,1 -k3rn,3 ${out}phe | awk '!seen[$1]++' > ${out}pheooO
 mv ${out}pheooO ${out}phe
 
-#TODO echo 
-
-
 cat ${out}phe | awk '{if($3 == "2"){print $1}}' | wc -l
 
 #Run Plink, make sure plink is installed
-#plink --bfile ${bfile} --assoc counts ${plinkSet} --pheno ${out}phe --out ${out}
+plink --bfile ${bfile} --assoc counts ${plinkSet} --pheno ${out}phe --out ${out}
+
 echo " "
 echo " " 
 #Run comparison between them
-python sprmanfull.py ${1} ${out}.assoc 8
-python sprmanOvP.py  ${1} ${out}.assoc 8 5 
-./sprmanTopN.sh      ${1} ${out}.assoc 8 100
+#python sprmanfull.py ${1} ${out}.assoc 8
+#python sprmanOvP.py  ${1} ${out}.assoc 8 5 
+#./sprmanTopN.sh      ${1} ${out}.assoc 8 100
+
+#exit
+#Make a manhattan plot
+lowestPVal=$(awk '{if(m== nulll || m > $9 && $9 != 0) {m = $9}} END{print m}' $1)
+echo "${lowestPVal}"
+typeTitle="Remade GWAS with children"
+hi=$(cat ${out}.assoc | sort -k9 -g | head -2 | tail -1 | awk '{print $11}'| sed -e 's/\// /g' | awk '{print $NF}'  | sed -e 's/\./ /g' | awk '{print $1}')
+echo "${hi}"
+grep "${hi}" ${out}.assoc | awk '{print $2}' > ${out}.list
+
+Rscript manPlot.r "${typeTitle}:${out}      Highest affect child: ${hi}" ${out}.assoc ${lowestPVal} ${out}.list
+
